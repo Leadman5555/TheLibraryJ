ee6dcaf28b5d944c9682be23f8e94b29971bd610
Ignacy Smoliński <ignacy.smolinski@gmail.com>
2024-11-28 18:29:52 +0100

    book preview grid

--- TheLibraryj/src/main/java/org/library/thelibraryj/book/domain/BookPreviewRepository.java
+++ TheLibraryj/src/main/java/org/library/thelibraryj/book/domain/BookPreviewRepository.java
@@ -3,6 +3,7 @@ package org.library.thelibraryj.book.domain;
 import io.hypersistence.utils.spring.repository.BaseJpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -20,21 +21,21 @@ interface BookPreviewRepository extends BaseJpaRepository<BookPreview, UUID> {
     List<BookPreview> getAllBookPreviewsEager();
 
     @Query("""
-            select bd from bookPreview bd
-            join fetch bd.bookTags
-            where bd.id = :id
+            select bp from bookPreview bp
+            join fetch bp.bookTags
+            where bp.id = :id
             """)
-    Optional<BookPreview> getBookPreviewEager(UUID id);
+    Optional<BookPreview> getBookPreviewEager(@Param("id") UUID id);
 
     @Query("""
     select (count(b) > 0) from bookPreview b where b.title = :title
     """)
-    boolean existsByTitle(String title);
+    boolean existsByTitle(@Param("title") String title);
 
     @Modifying
     @Query("""
     delete from bookPreview
     where id = :bookId
     """)
-    void deleteBook(UUID bookId);
+    void deleteBook(@Param("bookId") UUID bookId);
 }
