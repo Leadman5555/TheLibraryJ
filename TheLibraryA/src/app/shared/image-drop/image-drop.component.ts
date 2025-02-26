aedb64eba06588588ffe81775b56ed959af4fd3c
Ignacy Smoliński <ignacy.smolinski@gmail.com>
2025-02-26 19:10:36 +0100

    angular config changes

--- TheLibraryA/src/app/shared/image-drop/image-drop.component.ts
+++ TheLibraryA/src/app/shared/image-drop/image-drop.component.ts
@@ -1,6 +1,5 @@
 import {Component, Input} from '@angular/core';
 import {FormControl} from '@angular/forms';
-import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-image-drop',
